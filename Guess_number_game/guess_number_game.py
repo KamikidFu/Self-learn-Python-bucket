91462029bae5c312c1ce95345d326b1ca64897b4
Kamikid <kidfu@live.com>
2017-06-17 19:03:37 +1200

    Add comments to guess number game

--- Guess_number_game/guess_number_game.py
+++ Guess_number_game/guess_number_game.py
@@ -1,14 +1,18 @@
-import random
+#Using Pythong 3.6.1 on Windows 7
+#Kamikid, 17/Jun/17
+#API about random module is at https://docs.python.org/3.6/library/random.html#random.random
 
-restart="Y"
+import random   #Import random module for generating random number
 
-while(restart=="Y" or restart=="y"):
+restart="Y"     #Variable of restart game command
+
+while(restart=="Y" or restart=="y"):    #Only test if the restart is Y or y to run the game
     temp = input("Hello there, would you like to input a number between 0 to 9 inclusively to guess what your computer is thinking about?\n")
-    guess = random.randint(0,9)
-    if int(temp)==guess:
+    guess = random.randint(0,9)         #Pick up a random integer by randint method
+    if int(temp)==guess:                #Check the two number, using int() to parse str to int
         print("Yes, you are right!")
     else:
-        print("No, Computer is thinking about "+str(guess))
+        print("No, Computer is thinking about "+str(guess))#Use str() to parse int to str
     restart = input("Would you like to play again? Y/N\n")
 
 print("Game quit...")
