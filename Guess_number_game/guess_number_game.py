969b620804590131bcfa6ec698624cfc44728040
Kamikid <kidfu@live.com>
2017-06-17 20:51:17 +1200

    Upgrade to version 1.2

--- Guess_number_game/guess_number_game.py
+++ Guess_number_game/guess_number_game.py
@@ -1,23 +1,24 @@
 #Using Pythong 3.6.1 on Windows 7
 #Kamikid, 17/Jun/17
 #More about random module is at https://docs.python.org/3.6/library/random.html#random.random
-#Version:1.1
+#Version:1.2
 
 import random   #Import random module for generating random number
 
 restart="Y"     #Variable of restart game command
 guess = random.randint(0,9)         #Pick up a random integer by randint method
+guess_times = 3
 
-while(restart=="Y" or restart=="y"):    #Only test if the restart is Y or y to run the game
+while(guess_times!=0):    #Only test if the restart is Y or y to run the game
     temp = input("Hello there, would you like to input a number between 0 to 9 inclusively to guess what your computer is thinking about?\n")
     int_temp = int(temp)
     if int_temp==guess:                #Check the two number, using int() to parse str to int
         print("Yes, you are right!")
         break
     elif int_temp > guess:
-        print("The number you guessed is bigger than the computer thinks.")
+        print("The number you guessed is bigger than the computer thinks.\nThere are "+str(guess_times-1)+" chance(s) left")
     else:
-        print("The number you guessed is smaller than the computer thinks.")
-    restart = input("Would you like to play again? Y/N\n")
+        print("The number you guessed is smaller than the computer thinks\nThere are "+str(guess_times-1)+" chance(s) left")
+    guess_times-=1
 
 print("Game quit...")
