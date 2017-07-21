6cea3bbfe35d0c0dbd61252b0f6a537970ac9241
Kamikid <kidfu@live.com>
2017-07-21 13:30:40 +1200

    Add stack data structure

---
+++ Data_Structure/mystack.py
@@ -0,0 +1,19 @@
+#Basic implementation of stack with methods
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.item.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
