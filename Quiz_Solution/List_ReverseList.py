70fdf9376c7f5f27008f172593ab2bc692683c53
Kamikid <kidfu@live.com>
2017-07-21 18:45:35 +1200

    Add reverse list quiz and solution

--- Quiz_Solution/List_ReverseList.py
+++ Quiz_Solution/List_ReverseList.py
@@ -1,1 +1,17 @@
+#The key is the pointer trick and make it reversed
 
+def reverse(list):
+  cur = list.head
+  pre = None
+  nex = None
+  
+  while cur:
+    temp = cur.next
+    
+    cur.next = pre
+    
+    pre = cur
+    cur = temp
+  
+  list.head = pre
+  return list
